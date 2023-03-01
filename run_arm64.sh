46e4cd8c2b32f5d205f9dc4896b5195116477f99
Sameer Pujar <dev.spujar@gmail.com>
2023-03-01 10:01:16 +0000

    virtio: Add arm64 build support
    - Update target list to include arm64 platform as well.
      With this the QEMU is built now for both x86 and arm64.
    
    - Add script to launch linux guest on arm64 boards.
    
    Signed-off-by: Sameer Pujar <dev.spujar@gmail.com>

---
+++ run_arm64.sh
@@ -0,0 +1,11 @@
+./qemu/build/aarch64-softmmu/qemu-system-aarch64 \
+ -machine virt \
+ -cpu cortex-a57 \
+ -hda ./prebuilts/arm64/rootfs.ext4 \
+ -kernel ./prebuilts/arm64/Image \
+ -append "console=ttyAMA0 root=/dev/vda oops=panic panic_on_warn=1 panic=-1" \
+ -m 2048 \
+ -net nic,model=virtio \
+ -net user,hostfwd=tcp::10022-:22 \
+ -serial stdio -display none
+
