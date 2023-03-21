9151f9af617c5402f9e29ccce686f0d38e4badfc
Sameer Pujar <dev.spujar@gmail.com>
2023-03-21 06:03:49 +0000

    virtio: enable vhost path
    - update qemu to enable vhost path for virtio configs
    - also fix qemu launch script for arm64
    
    Signed-off-by: Sameer Pujar <dev.spujar@gmail.com>

--- run_arm64.sh
+++ run_arm64.sh
@@ -4,6 +4,7 @@
  -hda ./prebuilts/arm64/rootfs.ext4 \
  -kernel ./prebuilts/arm64/Image \
  -append "console=ttyAMA0 root=/dev/vda oops=panic panic_on_warn=1 panic=-1" \
+ -device virtio-snd-pci,disable-legacy=on \
  -m 2048 \
  -net nic,model=virtio \
  -net user,hostfwd=tcp::10022-:22 \
